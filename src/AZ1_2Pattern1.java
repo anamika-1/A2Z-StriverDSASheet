63df3d0241a429e860a99e229facce4a31630c1a
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2024-02-24 13:24:50 +0530

    Update AZ1_2Pattern1.java

--- src/AZ1_2Pattern1.java
+++ src/AZ1_2Pattern1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class AZ1_2Pattern1 {
     public static void main(String[] args) {
-        ystem.out.print("Enter a number: ");
+        System.out.print("Enter a number: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i = 0; i < n; i++){
