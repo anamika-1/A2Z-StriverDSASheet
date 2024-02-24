04316ec08d7433cd8c263260a122196b47815baa
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2024-02-24 13:22:35 +0530

    pattern

--- src/AZ1_2Pattern2.java
+++ src/AZ1_2Pattern2.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 public class AZ1_2Pattern2 {
     public static void main(String[] args) {
+        System.out.print("Enter a number: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
 
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= i; j++){
